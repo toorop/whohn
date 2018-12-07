db914a7cbfcd86dd3a9e8ce419d02567a2eaf4e7
Stéphane Depierrepont <toorop@toorop.fr>
2018-12-07 09:52:40 +0100

    clean code

--- lib/blocs/Webview_bloc.dart
+++ lib/blocs/Webview_bloc.dart
@@ -4,21 +4,16 @@ import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/subjects.dart';
 
 class WebviewBloc {
-  String _url = 'https://dpp.st/';
 
-  // Subject / sink
+  // Stream url
   final _url$ = BehaviorSubject<String>(seedValue: 'https://dpp.st/');
-
-  // Observable / stream
-  final _urlController = StreamController<String>.broadcast();
-  //final _urlController = DeferStream;
 
   Sink<String> get updateUrl => _url$.sink;
 
-  Stream<String> get url => _urlController.stream;
+  Stream<String> get url => _url$.stream;
 
   void dispose() {
-    _urlController.close();
+    //_urlController.close();
     _url$.close();
   }
 }
--- lib/screens/ShowNews.dart
+++ lib/screens/ShowNews.dart
@@ -4,12 +4,6 @@ import 'package:webview_flutter/webview_flutter.dart';
 import '../blocs/bloc_provider.dart';
 
 class ShowNews extends StatelessWidget {
-  //WebViewController _controller;
-
-  //final String _url;
-
-  //ShowNews(this._url);
-
   @override
   Widget build(BuildContext context) {
     final bloc = BlocProvider.of(context);
@@ -17,23 +11,18 @@ class ShowNews extends StatelessWidget {
       body: StreamBuilder(
           stream: bloc.url,
           builder: (context, snapshot) {
-            print("builder");
             if (snapshot.hasData) {
-              print("HAS DATA");
-              return Text(snapshot.data);
+              return WebView(
+                initialUrl: snapshot.data,
+                javaScriptMode: JavaScriptMode.unrestricted,
+                onWebViewCreated: (WebViewController _) {
+                  print('webviewcontroller created');
+                },
+              );
             } else {
               return CircularProgressIndicator();
             }
-          }
-
-          /*WebView(
-            initialUrl: _url,
-            javaScriptMode: JavaScriptMode.unrestricted,
-            onWebViewCreated: (WebViewController _) {
-              print('webviewcontroller created');
-            },
-          ),*/
-          ),
+          }),
     );
   }
 }
